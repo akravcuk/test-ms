6527b0147e27890bc5e115dbee549505250f5c10
Andriy Kravchuk <andriy.kravchuk@tatramed.sk>
2024-07-24 09:38:50 +0200

    abc

--- ms-2/app.py
+++ ms-2/app.py
@@ -4,7 +4,7 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET, POST'])
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
     return {
         "message" : "hello from second ms-service"
